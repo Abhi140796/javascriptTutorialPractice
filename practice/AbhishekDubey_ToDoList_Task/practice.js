7dbf86ae2b11e87b35e4826ab06c0d03a69b23ed
Abhi140796 <abhishekd905@gmail.com>
2022-02-07 00:34:10 +0530

    comments added for understanding

--- practice/AbhishekDubey_ToDoList_Task/practice.js
+++ practice/AbhishekDubey_ToDoList_Task/practice.js
@@ -9,35 +9,23 @@ let clearAllBtn = document.getElementById('clearAll');
 // Event Listener
 addBtn.addEventListener('click', function (event) {
     let textLength = textbox1.value.length;
-    // checking is input empty or not
     if (textLength != 0) {
-
-        // if not then
-        // create li element
         let li = document.createElement('li');
         li.classList.add('taskList');
-
-        // create button element
         let deleteBtn = document.createElement('button');
-
-        // setting style property
         list.style.border = '1px solid gainsboro';
         li.innerText = textbox1.value;
         deleteBtn.innerText = "X";
         deleteBtn.classList.add('deleteBtn');
-
-        // appending created elements
         list.appendChild(li);
         li.appendChild(deleteBtn);
         textbox1.value = "";
-
         // delete button event listener
         deleteBtn.addEventListener('click', () => {
             deleteBtn.parentElement.remove();
         });
     }
     else {
-        // if empty alert the user
         alert('\"Please enter a task\"');
     }
 });
